9a3e1459f4dac80c04bed5301e09bda737795753
Kassie Jeffery <kjeffery@davidsonacademy.unr.edu>
2024-05-04 23:09:11 +0000

    Correcting image loading and saving, testing of all functions, general code restructuring.

--- image.c
+++ image.c
@@ -2,133 +2,133 @@
 // Date: 4/29/24
 // Purpose: Image Processing
 #include<stdio.h>
+#include<stdbool.h>
 #define CAPACITY 500
+#define FILENAME_SIZE 50
 
-void saveImage(FILE* fptr, int rows, int cols, int colCapacity, int image[][colCapacity]);
-int loadImage(FILE* fptr, int* cols, int colCapacity, int image[][colCapacity]);
-void displayImage(int irow, int cols, int image[][CAPACITY]); 
-int editMenu(); 
-int cropImage(int cropRowStart, int cropRowEnd, int cropColStart, int cropColEnd, int rows, int* cols, int colCapacity, int image[][colCapacity]);
-void dimImage(int irow, int cols, int image[][CAPACITY]);
-void brightenImage(int irow, int cols, int image[][CAPACITY]);
+void saveImage(FILE* fptr, int rows, int cols, int image[][CAPACITY]);
+int loadImage(FILE* fptr, int* cols, int image[][CAPACITY]);
+char pixelChar(int brightness);
+void displayImage(int rows, int cols, int image[][CAPACITY]); 
+void editMenu(int* rows, int* cols, int image[][CAPACITY]); 
+int cropImage(int cropRowStart, int cropRowEnd, int cropColStart, int cropColEnd, int rows, int* cols, int image[][CAPACITY]);
+void dimImage(int rows, int cols, int image[][CAPACITY]);
+void brightenImage(int rows, int cols, int image[][CAPACITY]);
 
 int main() {
-    int userchoice1,userchoice2, image[CAPACITY][CAPACITY], cols, irow;
-    char userfile[50], usersave, usersavedfile[50];	
-    FILE *fptr, *fptr2;											//fptr1 = loaded file, fptr2 = user saved file
+    int selection, image[CAPACITY][CAPACITY], img_cols, img_rows;
+    char filename[FILENAME_SIZE];
+    bool file_loaded = false;
+    FILE* fptr;
     
-    do{
-    printf("**IMAGE PROCESSOR**\n");
-    printf("1: Load image\n");
-    printf("2: Display image\n");
-    printf("3: Edit image\n");
-    printf("0: Exit\n\n");
-    printf("Choose from one of the options above: ");
-    scanf("%d", &userchoice1);
-    
-    if(userchoice1 == 1){
-        printf("What is the name of the image file? ");
-        scanf("%s", userfile);
+    do {
+        printf("\n *** IMAGE PROCESSOR ***\n");
+        printf("1: Load image\n");
+        printf("2: Display image\n");
+        printf("3: Edit image\n");
+        printf("0: Exit\n\n");
+        printf("Choose from one of the options above: ");
+        scanf("%d", &selection);
         
-        fptr = fopen(userfile, "r");
-        if(fptr == NULL){
-            printf("Could not find image with that filename.\n");
-        }
-        else{
-        irow = loadImage(fptr, &cols, CAPACITY, image);
-        printf("\nImage successfully loaded!\n\n");
-        }
-    }
-    else if(userchoice1 == 2){
-         fptr = fopen(userfile, "r");
-        if(fptr == NULL){
-            printf("Sorry, no image to display\n");
-        }
-        else{
-        displayImage(irow, cols, image); 
-        }
-    }
-    else if(userchoice1 == 3){
-        fptr = fopen(userfile, "r");
-        if(fptr == NULL){
-            printf("Sorry, no image to edit\n");
+        if (selection == 1) {
+            printf("What is the name of the image file? ");
+            scanf("%s", filename);
             
+            fptr = fopen(filename, "r");
+            if (fptr == NULL) {
+                printf("\"%s\" file not found.\n", filename);
+                return 1;
+            } else{
+                img_rows = loadImage(fptr, &img_cols, image);
+                fclose(fptr);
+                printf("Image successfully loaded\n");
+                file_loaded = true;
+            }
+        } else if (selection != 2 || selection != 3) {
+            if (!file_loaded) {
+                printf("No image is currently loaded.\n");
+            } else if (selection == 2) {
+                displayImage(img_rows, img_cols, image);
+            } else if (selection == 3) {
+                editMenu(&img_rows, &img_cols, image);
+            }
+        } else if (selection != 0) {
+            printf("Invalid input.\n");
         }
-        else{
-        userchoice2 = editMenu();
-            if(userchoice2 == 1){
-                printf("RUN CROP IMAGE FUNCTION HERE\n");
-            }
-            else if(userchoice2 == 2){
-                
-                dimImage(irow, cols, image);			  					//Dims the image
-                printf("Would you like to save the file? (y/n) ");
-                scanf(" %c", &usersave);								//Store users option
-               
-                if(usersave == 'y'){
-                    printf("What do you want to name the image file? (include the extension) ");	//Ask user for file name if they enter 'y'
-                    scanf("%s", usersavedfile);								//Save users filename in an array
-                    
-                    fptr2 = fopen(usersavedfile, "w");							//Open the file for writing
-                    if(fptr2 == NULL){									//Best practice
-                        printf("Could not open file\n");
-                    }
-                    saveImage(fptr2, irow, cols, CAPACITY, image);					//Call Save Image function
-                    printf("\nImage Successfully saved!\n\n");						//Success Message
-                    fclose(fptr2);									//Close the file 
-                    irow = loadImage(fptr, &cols, CAPACITY, image);   					//Resets image back to original image
-                }
-                else{
-                    irow = loadImage(fptr, &cols, CAPACITY, image);  					//Resets image back to original image if it is not saved
-                }
-            }
-            else if(userchoice2 == 3){
-            
-                brightenImage(irow, cols, image);			  				//Dims the image
-                printf("Would you like to save the file? (y/n) ");
-                scanf(" %c", &usersave);
-               
-                if(usersave == 'y'){
-                    printf("What do you want to name the image file? (include the extension) ");
-                    scanf("%s", usersavedfile);
-                    
-                    fptr2 = fopen(usersavedfile, "w");
-                    if(fptr2 == NULL){
-                        printf("Could not open file\n");
-                    }
-                    saveImage(fptr2, irow, cols, CAPACITY, image);
-                    printf("\nImage Successfully saved!\n\n");
-                    fclose(fptr2);
-                    irow = loadImage(fptr, &cols, CAPACITY, image);   					//Resets image back to original image after file is closed
-                }
-                else{
-                    irow = loadImage(fptr, &cols, CAPACITY, image);   					//Resets image back to original image if it is not saved
-                }
-            }
-            else if("RETURN USER BACK TO MAIN MENU\n");	 						//This one was confusing because of how it works in the executable Erin provided
-        }
-        
-    }
-    
-    }while (userchoice1 != 0);
-    fclose(fptr);
-
-
-//Notes: 4-30-24
-   //Using the syntax in the imgdata file you provided the displayImage() function works, but it only works when there is spaces between the numbers. i tried using Erin's executable file example and it only counted it as one single column, so it ended up printing the whole 500x500 array.
-   
-//I Used the executable Erin provided as the basis for what i coded, so feel free to compare them and change stuff if you notice something is off (i stayed up coding and i was pretty tired so i probably made some mistakes somewhere). 
-
-//From what i tested in the code, so far it works and there are just a couple things to fix so it can match the executable. there are still some parts to finish like implementing the crop function into the main and  converting the numbers into symbols ( such as 0 = space, 2 = the letter o, etc). i will try to work more on it wednesday night but if not for sure thursday night. For the most part though we are almost done.
-
+    } while (selection != 0);
+    printf("Exiting program.\n");
 }
 
-void saveImage(FILE* fptr, int rows, int cols, int colCapacity, int image[][colCapacity]) {
+void editMenu(int* rows, int* cols, int image[][CAPACITY]){
+    int menu_option, cropBounds[4];
+    char selection, filename[FILENAME_SIZE];
+    FILE* fptr;
+    
+    do {
+        printf("\n *** IMAGE EDITOR ***\n");
+        printf("1: Crop image\n");
+        printf("2: Dim image\n");
+        printf("3: Brighten image\n");
+        printf("0: Return to main menu\n\n");
+        
+        printf("Choose from one of the options above: ");
+        scanf("%d", &menu_option);
+        
+        if (menu_option == 1) {
+            printf("Please enter the starting row (1+): ");
+            scanf("%d", &cropBounds[0]);
+            printf("Please enter the ending row (1+): ");
+            scanf("%d", &cropBounds[1]);
+            printf("Please enter the starting column (1+): ");
+            scanf("%d", &cropBounds[2]);
+            printf("Please enter the ending column (1+): ");
+            scanf("%d", &cropBounds[3]);
+            *rows = cropImage(cropBounds[0], cropBounds[1], cropBounds[2], cropBounds[3], *rows, cols, image);
+        } else if (menu_option == 2) {
+            dimImage(*rows, *cols, image); //Dims the image
+        } else if (menu_option == 3) {
+            brightenImage(*rows, *cols, image); //Dims the image
+        } else if (menu_option != 0) {
+            printf("Invalid input.\n");
+        }
+        
+        // If the image was edited
+        if (menu_option == 1 || menu_option == 2 || menu_option == 3) {
+            printf("New Image:\n\n");
+            displayImage(*rows, *cols, image);
+            printf("\nSave changes to a file (y/n)? ");
+            scanf(" %c", &selection);
+            
+            if (selection == 'y') {
+                printf("Enter the filename: ");
+                scanf("%s", filename);
+                
+                fptr = fopen(filename, "w");
+                if (fptr == NULL) {
+                    printf("Couldn't open \"%s\", changes not saved.\n", filename);
+                } else {
+                    saveImage(fptr, *rows, *cols, image);
+                    fclose(fptr);
+                    printf("\nImage successfully saved\n");
+                }
+                
+                printf("Returning to main menu.\n");
+                return;
+            }
+        }
+    } while (selection != 0);
+    printf("Returning to main menu.\n");
+}
+
+void saveImage(FILE* fptr, int rows, int cols, int image[][CAPACITY]) {
     // Iterate through every pixel
     for (int irow = 0; irow < rows; irow++) {
         for (int icol = 0; icol < cols; icol++) {
             // Store the current pixel brightness value
-            fprintf(fptr, "%d ", image[irow][icol]);
+            fprintf(fptr, "%d", image[irow][icol]);
+            if (icol < cols-1) {
+                fprintf(fptr, " ");
+            }
         }
         if (irow != rows-1) {
             fprintf(fptr, "\n");
@@ -136,7 +136,7 @@ void saveImage(FILE* fptr, int rows, int cols, int colCapacity, int image[][colC
     }
 }
 
-int loadImage(FILE* fptr, int* cols, int colCapacity, int image[][colCapacity]) {
+int loadImage(FILE* fptr, int* cols, int image[][CAPACITY]) {
     int irow, icol;
     irow = icol = 0;
     char separator;
@@ -146,7 +146,7 @@ int loadImage(FILE* fptr, int* cols, int colCapacity, int image[][colCapacity]) 
             // If the end of a row is reached, reset to the start of the next row
             if (irow == 0) {
                 // Assigns the new number of columns (checks if it's the first row to avoid multiple assignments)
-                *cols = icol;
+                *cols = icol+1;
             }
             icol = 0;
             irow++;
@@ -156,35 +156,34 @@ int loadImage(FILE* fptr, int* cols, int colCapacity, int image[][colCapacity]) 
         }
     }
     // Return the new image height
-    return irow;
+    return irow+1;
 }
 
-void displayImage(int irow, int cols, int image[][CAPACITY]){
-    for(int i = 0; i < irow; i++){
+char pixelChar(int brightness) {
+    switch (brightness) {
+        case 0:
+            return ' ';
+        case 1:
+            return '.';
+        case 2:
+            return 'o';
+        case 3:
+            return 'O';
+        case 4:
+            return '0';
+    }
+}
+
+void displayImage(int rows, int cols, int image[][CAPACITY]){
+    for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
-            printf("%d", image[i][j]);
+            printf("%c ", pixelChar(image[i][j]));
         }
         printf("\n");
     }
 }
 
-int editMenu(){
-    int userchoice2;
-    
-    printf("**EDITING**\n");
-    printf("1: Crop image\n");
-    printf("2: Dim image\n");
-    printf("3: Brighten image\n");
-    printf("0: Return to main menu\n\n");
-    
-    printf("Choose from one of the options above: ");
-    scanf("%d", &userchoice2);
-    
-    return userchoice2;
-    
-}
-
-int cropImage(int cropRowStart, int cropRowEnd, int cropColStart, int cropColEnd, int rows, int* cols, int colCapacity, int image[][colCapacity]) {
+int cropImage(int cropRowStart, int cropRowEnd, int cropColStart, int cropColEnd, int rows, int* cols, int image[][CAPACITY]) {
     // Check to see if the start and end indexes are backwards
     if (cropRowStart >= cropRowEnd || cropColStart >= cropColEnd) {
         printf("Starting rows and columns must come before ending rows and columns!\n");
@@ -193,7 +192,7 @@ int cropImage(int cropRowStart, int cropRowEnd, int cropColStart, int cropColEnd
     }
     
     // Checks to see if start indexes are less than 0
-    if (cropRowStart < 0 || cropColStart < 0) {
+    if (cropRowStart < 1 || cropColStart < 1) {
         printf("Starting row and column must be at least 1!\n");
         printf("Terminating image cropper\n");
         return rows;
@@ -231,23 +230,27 @@ int cropImage(int cropRowStart, int cropRowEnd, int cropColStart, int cropColEnd
     // Return the new number of rows for external use
     return height;
 }
-void dimImage(int irow, int cols, int image[][CAPACITY]){
-    for(int i = 0; i < irow; i++){
+
+void dimImage(int rows, int cols, int image[][CAPACITY]){
+    for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
-            image[i][j]--;					//Subtracts one from each "pixel"
-         
+            image[i][j]--; // Decreases the brightness of each pixel
+            if (image[i][j] < 0) {
+                image[i][j] = 0;
+            }
         }
     }
-    displayImage(irow, cols, image);				//Needs to display "new" image
 }
-void brightenImage(int irow, int cols, int image[][CAPACITY]){
-    for(int i = 0; i < irow; i++){
+
+void brightenImage(int rows, int cols, int image[][CAPACITY]){
+    for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
-            image[i][j]++;					//adds one to each "pixel"
-         
+            image[i][j]++; // Increases the brightness of each pixel
+            if (image[i][j] > 4) {
+                image[i][j] = 4;
+            }
         }
     }
-    displayImage(irow, cols, image);				//Needs to display "new" image
 }
 
 
